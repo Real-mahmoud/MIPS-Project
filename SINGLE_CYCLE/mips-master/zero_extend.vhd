df28e6cb88bc681557c7c743a9efd92cdf95ac9f
mahmoud khaled <155483657+Real-mahmoud@users.noreply.github.com>
2025-12-18 07:18:07 +0200

    Update zero_extend.vhd

--- SINGLE_CYCLE/mips-master/zero_extend.vhd
+++ SINGLE_CYCLE/mips-master/zero_extend.vhd
@@ -1,24 +1,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity zero_extend is
-    Port ( inic : in  STD_LOGIC;
-           extend : out  STD_LOGIC_VECTOR (31 downto 0));
+    Port (
+        inic   : in  STD_LOGIC;
+        extend : out STD_LOGIC_VECTOR (31 downto 0)
+    );
 end zero_extend;
 
 architecture Behavioral of zero_extend is
-
 begin
-	extend<=x"0000000"&"000"&inic;
-
+    extend <= (31 downto 1 => '0') & inic;
 end Behavioral;
-
