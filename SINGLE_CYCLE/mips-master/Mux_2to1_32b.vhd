0cbdad23a502ad409b720da57a1cb980ce3dd4d2
mahmoud khaled <155483657+Real-mahmoud@users.noreply.github.com>
2025-12-18 08:06:11 +0200

    Clean up commented library declarations
    Removed commented library declarations for clarity.

--- SINGLE_CYCLE/mips-master/Mux_2to1_32b.vhd
+++ SINGLE_CYCLE/mips-master/Mux_2to1_32b.vhd
@@ -1,16 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- Bottazzi, Ferrer
---use IEEE.NUMERIC_STD.ALL;
--- Bottazzi, Cristian - 2017 (https://github.com/cristian1604/)
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Mux_2to1_32b is
     Port ( ctrl : in  STD_LOGIC;
            A : in  STD_LOGIC_VECTOR (31 downto 0);
