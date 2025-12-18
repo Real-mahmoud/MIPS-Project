b0e6bbe901ee06b33ee30ea1e35e6e0f85e17e57
mahmoud khaled <155483657+Real-mahmoud@users.noreply.github.com>
2025-12-18 07:24:00 +0200

    Update corrimiento.vhd

--- SINGLE_CYCLE/mips-master/corrimiento.vhd
+++ SINGLE_CYCLE/mips-master/corrimiento.vhd
@@ -1,24 +1,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
--- Bottazzi, Cristian - 2017 (https://github.com/cristian1604/)
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity corrimiento is
-    Port ( din : in  STD_LOGIC_VECTOR (31 downto 0);
-           dout : out  STD_LOGIC_VECTOR (31 downto 0));
+    Port (
+        din  : in  STD_LOGIC_VECTOR (31 downto 0);
+        dout : out STD_LOGIC_VECTOR (31 downto 0)
+    );
 end corrimiento;
 
 architecture Behavioral of corrimiento is
-
 begin
-	dout<=din(29 downto 0)&"00"; --es una multiplicacion por 4
+    dout <= din(29 downto 0) & "00"; -- shift left 2 (x4)
 end Behavioral;
-
