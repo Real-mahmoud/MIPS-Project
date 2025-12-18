a6edc8ca1f087e8566fa21fe871262d862b94c92
mahmoud khaled <155483657+Real-mahmoud@users.noreply.github.com>
2025-12-18 08:07:38 +0200

    Clean up commented code in SignExtend.vhd
    Removed commented library declarations from SignExtend.vhd.

--- SINGLE_CYCLE/mips-master/SignExtend.vhd
+++ SINGLE_CYCLE/mips-master/SignExtend.vhd
@@ -1,16 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
--- Bottazzi, Cristian - 2017 (https://github.com/cristian1604/)
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity SignExtend is
     Port ( din : in  STD_LOGIC_VECTOR (15 downto 0);
            dout : out  STD_LOGIC_VECTOR (31 downto 0));
