b7dfc905788da1d9ead024331d43a23a18c765ad
mahmoud khaled <155483657+Real-mahmoud@users.noreply.github.com>
2025-12-18 08:07:21 +0200

    Clean up comments in PC.vhd
    Removed commented library declarations from PC.vhd

--- SINGLE_CYCLE/mips-master/PC.vhd
+++ SINGLE_CYCLE/mips-master/PC.vhd
@@ -1,16 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
--- Bottazzi, Cristian - 2017 (https://github.com/cristian1604/)
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity PC is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
